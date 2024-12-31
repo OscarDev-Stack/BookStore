657b8e3f6a374a655faf48bae83207315273d825
OscarJRuiz29 <133481203+OscarJRuiz29@users.noreply.github.com>
2024-12-31 01:54:24 -0600

    inicio

---
+++ src/BookStore.Api/Controllers/BookController.cs
@@ -0,0 +1,48 @@
+﻿using BookStore.Entities;
+using BookStore.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookRepository repository;
+
+        public BookController(BookRepository repository)
+        {
+            this.repository = repository;
+        }
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            var data = repository.Get();
+            return Ok(data);
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<Book> Get(int id)
+        {
+            var item = repository.Get(id);
+            return item is not null ? Ok(item) : NotFound();
+        }
+        [HttpPost]
+        public ActionResult<Book> Post(Book book)
+        {
+            repository.Add(book);
+            return Ok(book);
+        }
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Book book)
+        {
+            repository.Update(id, book);
+            return Ok();
+        }
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ src/BookStore.Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.Entities;
+public class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Author { get; set; } = default!;
+    public string ISBN { get; set; } = default!;
+    public string Editorial { get; set; } = default!;
+    public string Synopsis { get; set; } = default!;
+    public bool Status { get; set; } = true;
+}
